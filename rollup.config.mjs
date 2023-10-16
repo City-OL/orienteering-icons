5a7c9c0f1ba0d7dec7871732ce42737807b64386
timmedia <contact@tim-media.com>
2023-10-16 20:20:47 +0200

    build: add rollup bundler build step

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,24 +4,24 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import summary from 'rollup-plugin-summary';
-import {terser} from 'rollup-plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import summary from "rollup-plugin-summary";
+import replace from "@rollup/plugin-replace";
 
 export default {
-  input: 'my-element.js',
+  input: "lib/orienteering-icon.js",
   output: {
-    file: 'my-element.bundled.js',
-    format: 'esm',
+    file: "lib/orienteering-icon.bundled.js",
+    format: "esm",
   },
   onwarn(warning) {
-    if (warning.code !== 'THIS_IS_UNDEFINED') {
+    if (warning.code !== "THIS_IS_UNDEFINED") {
       console.error(`(!) ${warning.message}`);
     }
   },
   plugins: [
-    replace({'Reflect.decorate': 'undefined'}),
+    replace({ "Reflect.decorate": "undefined" }),
     resolve(),
     /**
      * This minification setup serves the static site generation.
